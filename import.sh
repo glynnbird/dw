67352bdb71f231fea7b86a2af8cafa9e51ac1ef8
Glynn Bird <glynn.bird@gmail.com>
2015-04-22 16:11:11 +0100

    tweek

--- import.sh
+++ import.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+WD=`pwd`
 export COUCH_URL=https://reader.cloudant.com
 export COUCH_DATABASE=dw
-export COUCH_TRANSFORM=/Users/glynnb/projects/dw/transform.js
+export COUCH_TRANSFORM="$WD/transform.js"
 cat raw.tsv | couchimport
+node spider.js
