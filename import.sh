6abad5dde577cb5fde17eab84f36e91079dd43cf
Glynn Bird <glynn.bird@gmail.com>
2015-04-22 16:16:21 +0100

    tidy up

--- import.sh
+++ import.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-WD=`pwd`
+WORKINGDIR=`pwd`
 export COUCH_URL=https://reader.cloudant.com
 export COUCH_DATABASE=dw
-export COUCH_TRANSFORM="$WD/transform.js"
+export COUCH_TRANSFORM="$WORKINGDIR/transform.js"
 cat raw.tsv | couchimport
 node spider.js
+ccurl -X POST /dw -d @index.json
